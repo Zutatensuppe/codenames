a9598ea1c540a2204854d8e6c1e44abc7e897c04
Zutatensuppe <oo.para@gmail.com>
2019-07-15 18:41:59 +0200

    better game

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
+const rand = (min, max) => Math.floor((Math.random() * max) + min)
+const make = (text, color) => {
+    const el = document.createElement('div')
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = text
+    el.classList.add('word', color)
+    el.appendChild(wrapper)
+    wrapper.classList.add('wrapper')
+    return el
+}
+const shuffle = array => {
+    let counter = array.length;
+    while (counter > 0) {
+        let index = rand(0, counter)
+        counter--;
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+    return array;
+}
+
+const sample = (array, count) => {
+    const indexes = []
+    while (indexes.length < count) {
+        let num = rand(0, array.length - 1)
+        if (indexes.includes(num)) {
+            continue;
+        }
+        indexes.push(num)
+    }
+    return array.filter((v, i) => indexes.includes(i))
+}
+
+const pad = (txt, len) => (' '.repeat(len) + txt).slice(-len)
+
+const limit = (conf, key) => {
+    let l = 0
+    for (let k of Object.keys(conf)) {
+        l += conf[k].count
+        if (k === key) {
+            return l
+        }
+    }
+    return l
+}
+
+const total = (conf) => {
+    let l = 0
+    for (let k of Object.keys(conf)) {
+        l += conf[k].count
+    }
+    return l
+}
+
 const words = [
     'Acne', 'Acre', 'Addendum', 'Advertise', 'Aircraft', 'Aisle', 'Alligator', 'Alphabetize', 'America', 'Ankle', 'Apathy', 'Applause', 'Applesauc', 'Application', 'Archaeologist', 'Aristocrat', 'Arm', 'Armada', 'Asleep', 'Astronaut', 'Athlete', 'Atlantis', 'Aunt', 'Avocado', 'Baby-Sitter', 'Backbone', 'Bag', 'Baguette', 'Bald', 'Balloon', 'Banana', 'Banister', 'Baseball', 'Baseboards', 'Basketball', 'Bat', 'Battery', 'Beach', 'Beanstalk', 'Bedbug', 'Beer', 'Beethoven', 'Belt', 'Bib', 'Bicycle', 'Big', 'Bike', 'Billboard', 'Bird', 'Birthday', 'Bite', 'Blacksmith', 'Blanket', 'Bleach', 'Blimp', 'Blossom', 'Blueprint', 'Blunt', 'Blur', 'Boa', 'Boat', 'Bob', 'Bobsled', 'Body', 'Bomb', 'Bonnet', 'Book', 'Booth', 'Bowtie', 'Box', 'Boy', 'Brainstorm', 'Brand', 'Brave', 'Bride', 'Bridge', 'Broccoli', 'Broken', 'Broom', 'Bruise', 'Brunette', 'Bubble', 'Buddy', 'Buffalo', 'Bulb', 'Bunny', 'Bus', 'Buy', 'Cabin', 'Cafeteria', 'Cake', 'Calculator', 'Campsite', 'Can', 'Canada', 'Candle', 'Candy', 'Cape', 'Capitalism', 'Car', 'Cardboard', 'Cartography', 'Cat', 'Cd', 'Ceiling', 'Cell', 'Century', 'Chair', 'Chalk', 'Champion', 'Charger', 'Cheerleader', 'Chef', 'Chess', 'Chew', 'Chicken', 'Chime', 'China', 'Chocolate', 'Church', 'Circus', 'Clay', 'Cliff', 'Cloak', 'Clockwork', 'Clown', 'Clue', 'Coach', 'Coal', 'Coaster', 'Cog', 'Cold', 'College', 'Comfort', 'Computer', 'Cone', 'Constrictor', 'Continuum', 'Conversation', 'Cook', 'Coop', 'Cord', 'Corduroy', 'Cot', 'Cough', 'Cow', 'Cowboy', 'Crayon', 'Cream', 'Crisp', 'Criticize', 'Crow', 'Cruise', 'Crumb', 'Crust', 'Cuff', 'Curtain', 'Cuticle', 'Czar', 'Dad', 'Dart', 'Dawn', 'Day', 'Deep', 'Defect', 'Dent', 'Dentist', 'Desk', 'Dictionary', 'Dimple', 'Dirty', 'Dismantle', 'Ditch', 'Diver', 'Doctor', 'Dog', 'Doghouse', 'Doll', 'Dominoes', 'Door', 'Dot', 'Drain', 'Draw', 'Dream', 'Dress', 'Drink', 'Drip', 'Drums', 'Dryer', 'Duck', 'Dump', 'Dunk', 'Dust', 'Ear', 'Eat', 'Ebony', 'Elbow', 'Electricity', 'Elephant', 'Elevator', 'Elf', 'Elm', 'Engine', 'England', 'Ergonomic', 'Escalator', 'Eureka', 'Europe', 'Evolution', 'Extension', 'Eyebrow', 'Fan', 'Fancy', 'Fast', 'Feast', 'Fence', 'Feudalism', 'Fiddle', 'Figment', 'Finger', 'Fire', 'First', 'Fishing', 'Fix', 'Fizz', 'Flagpole', 'Flannel', 'Flashlight', 'Flock', 'Flotsam', 'Flower', 'Flu', 'Flush', 'Flutter', 'Fog', 'Foil', 'Football', 'Forehead', 'Forever', 'Fortnight', 'France', 'Freckle', 'Freight', 'Fringe', 'Frog', 'Frown', 'Gallop', 'Game', 'Garbage', 'Garden', 'Gasoline', 'Gem', 'Ginger', 'Gingerbread', 'Girl', 'Glasses', 'Goblin', 'Gold', 'Goodbye', 'Grandpa', 'Grape', 'Grass', 'Gratitude', 'Gray', 'Green', 'Guitar', 'Gum', 'Gumball', 'Hair', 'Half', 'Handle', 'Handwriting', 'Hang', 'Happy', 'Hat', 'Hatch', 'Headache', 'Heart', 'Hedge', 'Helicopter', 'Hem', 'Hide', 'Hill', 'Hockey', 'Homework', 'Honk', 'Hopscotch', 'Horse', 'Hose', 'Hot', 'House', 'Houseboat', 'Hug', 'Humidifier', 'Hungry', 'Hurdle', 'Hurt', 'Hut', 'Ice', 'Implode', 'Inn', 'Inquisition', 'Intern', 'Internet', 'Invitation', 'Ironic', 'Ivory', 'Ivy', 'Jade', 'Japan', 'Jeans', 'Jelly', 'Jet', 'Jig', 'Jog', 'Journal', 'Jump', 'Key', 'Killer', 'Kilogram', 'King', 'Kitchen', 'Kite', 'Knee', 'Kneel', 'Knife', 'Knight', 'Koala', 'Lace', 'Ladder', 'Ladybug', 'Lag', 'Landfill', 'Lap', 'Laugh', 'Laundry', 'Law', 'Lawn', 'Lawnmower', 'Leak', 'Leg', 'Letter', 'Level', 'Lifestyle', 'Ligament',
     'Light', 'Lightsaber', 'Lime', 'Lion', 'Lizard', 'Log', 'Loiterer', 'Lollipop', 'Loveseat', 'Loyalty', 'Lunch', 'Lunchbox', 'Lyrics', 'Machine', 'Macho', 'Mailbox', 'Mammoth', 'Mark', 'Mars', 'Mascot', 'Mast', 'Matchstick', 'Mate', 'Mattress', 'Mess', 'Mexico', 'Midsummer', 'Mine', 'Mistake', 'Modern', 'Mold', 'Mom', 'Monday', 'Money', 'Monitor', 'Monster', 'Mooch', 'Moon', 'Mop', 'Moth', 'Motorcycle', 'Mountain', 'Mouse', 'Mower', 'Mud', 'Music', 'Mute', 'Nature', 'Negotiate', 'Neighbor', 'Nest', 'Neutron', 'Niece', 'Night', 'Nightmare', 'Nose', 'Oar', 'Observatory', 'Office', 'Oil', 'Old', 'Olympian', 'Opaque', 'Opener', 'Orbit', 'Organ', 'Organize', 'Outer', 'Outside', 'Ovation', 'Overture', 'Pail', 'Paint', 'Pajamas', 'Palace', 'Pants', 'Paper', 'Paper', 'Park', 'Parody', 'Party', 'Password', 'Pastry', 'Pawn', 'Pear', 'Pen', 'Pencil', 'Pendulum', 'Penis',
@@ -6,137 +61,36 @@ const words = [
     'Wax', 'Wedding', 'Weed', 'Welder', 'Whatever', 'Wheelchair', 'Whiplash', 'Whisk', 'Whistle', 'White', 'Wig', 'Will', 'Windmill', 'Winter', 'Wish', 'Wolf', 'Wool', 'World', 'Worm', 'Wristwatch', 'Yardstick', 'Zamboni', 'Zen', 'Zero', 'Zipper', 'Zone', 'Zoo',
 ]
 
-const gamediv = document.getElementById('game')
-const rand = (min, max) => Math.floor((Math.random() * max) + min)
-const make = (text, color) => {
-    const el = document.createElement('div')
-    el.classList.add('word', color)
-    el.innerHTML = text
-    return el
+const conf = {
+    bad: {color: '#3f3e37', count: 1},
+    own: {color: '#107fa6', count: 8},
+    enemy: {color: '#e73c3f', count: 9},
+    neutral: {color: '#a1856e', count: 7},
 }
 
-const indexes = []
-while (indexes.length < 25) {
-    let num = rand(0, words.length - 1)
-    if (indexes.includes(num)) {
-        continue;
+let longest = 0
+const shuffled = shuffle(sample(words, total(conf)).map((word, i) => {
+    longest = Math.max(longest, word.length)
+    for (let k of Object.keys(conf)) {
+        if (i < limit(conf, k)) {
+            return {type: k, text: word};
+        }
     }
-    indexes.push(num)
-}
+}))
+longest += 2 // padding
 
-function shuffle(array) {
-    let counter = array.length;
+const gameEl = document.getElementById('game')
 
-    // While there are elements in the array
-    while (counter > 0) {
-        // Pick a random index
-        let index = Math.floor(Math.random() * counter);
-
-        // Decrease counter by 1
-        counter--;
-
-        // And swap the last element with it
-        let temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
+let msgs = []
+let clrs = []
+shuffled.forEach(item => {
+    const el = make(item.text, item.type)
+    gameEl.appendChild(el)
+    el.onclick = (ev) => {
+        ev.currentTarget.classList.add('revealed')
     }
 
-    return array;
-}
-
-let where = 0
-function next(arr, count) {
-    n = arr.slice(where, where + count)
-    where += count
-    return n
-}
-
-const picked = indexes.map(idx => words[idx])
-console.log(picked)
-const bad = next(picked, 1)
-const own = next(picked, 8)
-const enemy = next(picked, 9)
-const neutral = next(picked, 7)
-
-console.log(bad, bad.length)
-console.log(own, own.length)
-console.log(enemy, enemy.length)
-console.log(neutral, enemy.length)
-
-const elements = []
-bad.forEach(item => {
-    elements.push(make(item, 'bad'))
+    msgs.push(`%c${pad(item.text, longest)}${(msgs.length + 1) % 5 ? '' : '\n'}`)
+    clrs.push(`color: ${conf[item.type].color}`)
 })
-own.forEach(item => {
-    elements.push(make(item, 'own'))
-})
-enemy.forEach(item => {
-    elements.push(make(item, 'enemy'))
-})
-neutral.forEach(item => {
-    elements.push(make(item, 'neutral'))
-})
-const shuffled = shuffle(elements)
-shuffled.forEach(el => {
-    gamediv.appendChild(el)
-})
-
-button = document.createElement('div')
-button.innerHTML = 'open secret'
-document.body.appendChild(button)
-let secret = null
-button.onclick= function() {
-    if (secret != null) {
-        secret.close()
-    }
-    secret = window.open('', 'secret')
-    secret.document.write(`
-    <meta charset="utf-8" />
-    <style>
-        body {
-            font-family: sans-serif;
-            font-weight: bold;
-        }
-        .bad {
-            color: #3f3e37;
-        }
-        .bad:before {
-            content: "🏴"
-        }
-
-        .own {
-            color: #107fa6
-        }
-        .own:before {
-            content: " 🔵 "
-        }
-
-        .enemy {
-            color: #e73c3f
-        }
-        .enemy:before {
-            content: "♦️"
-        }
-
-        .neutral {
-            color: #a1856e
-        }
-
-        .neutral:before {
-            content: "🥔"
-        }
-
-        #game {
-            display: grid;
-            grid-template-columns: auto auto auto auto auto;
-            grid-gap: 10px;
-        }
-        .word {
-            border: solid 1px black;
-            background: #e4ceb3;
-            padding: 10px;
-            border-radius: 5px;
-        }
-    </style>`)
-    secret.document.write(gamediv.outerHTML)
-}
+console.log(msgs.join(''), ...clrs);
